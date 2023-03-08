fe7bc74f2aa3e3e93487ef396b8ad384b1639d55
TheSwordSlay <107758410+TheSwordSlay@users.noreply.github.com>
2023-03-08 09:36:57 +0700

    first commit

---
+++ absen_ai.php
@@ -0,0 +1,74 @@
+<?php
+require 'functions.php';
+$conn = mysqli_connect("localhost", "root", "", "absensi");
+
+function submitAbsen ($data) {
+    //ambil data dari input form
+    global $conn;
+    $namaMa = $data["namaMa"];
+    $nim = $data["nim"];
+    $kehadiran = $data["kehadiran"];
+    $tanggal = $data["tanggal"];
+
+    //query insert data
+    $query = "INSERT INTO kecerdasan_buatan VALUES (
+        '$namaMa',
+        '$nim',
+        '$kehadiran',
+        CURRENT_DATE()
+        )";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+if( isset($_POST["submit"]) ) {
+    //cek apakah data berhasil di tambahkan atau tidak
+    if( submitAbsen($_POST) > 0 ) {
+        echo "
+            <script>
+                alert('Absen berhasil')
+                document.location.href = 'index.php';
+            </script>
+        ";
+    } else {
+        echo "
+            <script>
+                alert('absen gagal :(');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Absensi Kecerdasan Buatan</title>
+</head>
+<body>
+<form action="" method="post">
+        <ul>
+            <li>
+                <label for="namaMa">Nama</label>
+                <input type="text" name="namaMa" id="namaMa" required>
+            </li>
+            <li>
+                <label for="nim">Nim</label>
+                <input type="text" name="nim" id="nim" required>
+            </li>
+            <li>
+                <label for="kehadiran">Kehadiran</label>
+                <input type="text" name="kehadiran" id="kehadiran" required>
+            </li>
+        </ul>
+        <button type="submit" name="submit">Submit absen!</button>
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Jadwal</title>
+        <link rel="stylesheet" href="assets/style.css"
+    </head>
+    <body>
+        <header>
+            <h1>TI Reg C</h1>
+            <nav>
+                <a href="#">Jadwal</a>
+                <!-- <button>Tugas</button> -->
+                <a href="#">Absensi</a>
+            </nav>
+        </header>
+        <div clas="content-box">
+            <div class="first-line direction">
+                <div class="schedule-card">
+                    <h1 class="day">Senin</h1>
+                    <section class="subjects">
+                        <a href="#">Pengolahan Citra</a>
+                        <a href="#">10.30 - 13.00</a>
+                    </section>
+                </div>
+                <div class="schedule-card">
+                    <h1 class="day">Selasa</h1>
+                    <section class="subjects">
+                        <a href="#">Kecerdasan Buatan</a>
+                        <a href="#">08.00 - 10.30</a>
+                    </section>
+                    <section class="subjects">
+                        <a href="#">PPLBO</a>
+                        <a href="#">13.30 - 16.00</a>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
