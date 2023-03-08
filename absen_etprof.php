f07be2a9f9ec4a7f9d95d56063d342acfc70adef
Marsa Qurratu'ain <108612059+MarsaQain@users.noreply.github.com>
2023-03-08 10:25:54 +0700

    Update absen_etprof.php
    P1

--- absen_etprof.php
+++ absen_etprof.php
@@ -50,7 +50,7 @@ if( isset($_POST["submit"]) ) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Absensi etika profesi</title>
+    <title>Absensi etika profesis</title>
 </head>
 <body>
 <form action="" method="post">
