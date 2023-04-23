b73d2ccf1d334e760524b9354d88ab906268615a
TheSwordSlay <107758410+TheSwordSlay@users.noreply.github.com>
2023-04-23 23:26:16 +0700

    fixed title and search route on daftar matkul

--- resources/views/absen.blade.php
+++ resources/views/absen.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>TI Reg C 21</title>
 </head>
 <body class="bg-slate-200">
     @include('partials.navbartailwind')
